41d2c51c7c81ea0ac21f8cbce41d9108e52529f8
Daniel Mihai <sasdaniel@daniels-mbp.home>
2022-09-14 13:22:51 +0100

    Repaired empty array error

--- src/Helpers/getCreatedHouses.js
+++ src/Helpers/getCreatedHouses.js
@@ -4,7 +4,10 @@ import API_REQUEST from "../Axios/axios"
 export const GET_CREATED_HOUSES = async _id => {
     try {
         const houses = await API_REQUEST.get(`/get-created-houses/${_id}`);
-        return houses;    
+            if (houses.length === 0) 
+                return 0;
+            else 
+                return houses;
     } 
     catch (error) {
         return error;
