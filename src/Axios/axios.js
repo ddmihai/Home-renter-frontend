a299bdfdf2f8bb4fecb4d9eb0d32e3cd90c539a0
Daniel Mihai <sasdaniel@daniels-mbp.home>
2022-09-15 14:10:32 +0100

    changed to cloudinary images

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_REQUEST = axios.create({
     // baseURL: 'https://home-renter-backend.herokuapp.com/api'
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://home-renter-backend.herokuapp.com/api'
   });
 
   export default API_REQUEST; 
